1c804ef505db4c111134eabd54a964ba22412196
Enrique Vigil <evigil.gar@gmail.com>
2021-03-07 19:19:05 -0600

    dos noticias agregadas

--- js/news.js
+++ js/news.js
@@ -9,10 +9,133 @@ let tags = [
     { name: 'PostgreSQL' },
     { name: 'Linux' },
     { name: 'Windows' },
-    { name: 'Java' }
+    { name: 'Java' },
+    { name: 'Diseño'},
 ];
 
 let news = [
+    {
+        id: '15982b18-ff71-4f4b-9bb9-bf0b452e27c6',
+        title: 'Eventos mensuales del año',
+        autor: 'Enrique Vigil',
+        date: '07/03/2021',
+        tags: [tags[11]],
+        read: '7 min',
+        likes: 4,
+        content: `
+           
+            <p>Tarea de eventos mensuales en el a&ntilde;o. Para multimedios</p>
+            <h3><strong>Enero</strong></h3>
+
+            <p>
+                <img src="../img/Desktop-1.png" alt="img" width="600" />
+            </p>
+
+            <p>P&uacute;blico objetivo: personas de cualquier edad amantes de la celebraci&oacute;n de a&ntilde;o nuevo. Se utiliz&oacute; esta imagen ya que hace representaci&oacute;n a la alegr&iacute;a y fervor que representa el comienzo de un nuevo a&ntilde;o.&nbsp;</p>
+            <h3><strong>Febrero</strong></h3>
+            
+            <p>
+                <img src="./img/Desktop-2.png" alt="img" width="600" />
+            </p>
+
+            <p>P&uacute;blico objetivo: personas de cualquier sexo que celebran la importancia del amor y la amistad Se utiliz&oacute; esta imagen ya que hace representaci&oacute;n a los amantes que celebran con gran entusiasmo y alegr&iacute;a el D&iacute;a de San Valent&iacute;n.</p>
+            <h3><strong>Marzo</strong></h3>
+
+            <p>
+                <img src="./img/Desktop-3.png" alt="img" width="600" />
+            </p>
+
+            <p>P&uacute;blico objetivo: personas del sexo femenino. Se utiliz&oacute; esta imagen ya que hace representaci&oacute;n al d&iacute;a de la mujer que ayuda a sensibilizar a la poblaci&oacute;n, gobiernos y Estados sobre la existencia de problemas que atentan contra los derechos de las mujeres.&nbsp;</p>
+            <h3><strong>Abril</strong></h3>
+
+            <p>
+                <img src="./img/Desktop-4.png" alt="img" width="600" />
+            </p>
+
+            <p>P&uacute;blico objetivo: Feligreses cat&oacute;licos que conmemoran la pasi&oacute;n de cristo Se utiliz&oacute; esta imagen ya que hace representaci&oacute;n a todo el mundo la pasi&oacute;n, muerte y resurrecci&oacute;n de Jes&uacute;s de Nazaret durante los ocho d&iacute;as que se lleva a cabo.</p>
+            <h3><strong>Mayo</strong></h3>
+
+            <p>
+                <img src="./img/Desktop-5.png" alt="img" width="600" />
+            </p>
+
+            <p>P&uacute;blico objetivo: Personas del sexo femenino, que son madres o est&aacute;n en camino a serlo. Se utiliz&oacute; esta imagen ya que hace representaci&oacute;n a una mujer que est&aacute; a punto de convertirse en madre y dar vida a un nuevo ser humano.</p>
+            <h3><strong>Junio</strong></h3>
+            <p>
+                <img src="./img/Desktop-6.png" alt="img" width="600" />
+            </p>
+            <p>P&uacute;blico objetivo: Personas del sexo masculino que tienen la dicha de ser padres. Se utiliz&oacute; esta imagen ya que hace referencia a un padre d&aacute;ndole amor y protecci&oacute;n a su hijo.</p>
+            <h3><strong>Julio</strong></h3>
+            <p>
+                <img src="./img/Desktop-7.png" alt="img" width="600" />
+            </p>
+            <p>Publico objetivo: Personas de cualquier sexo y edad que aman a los perros y cuidan de estos. Se utiliz&oacute; esta imagen porque hace referencia a unos cachorros felices y su amo conviviendo con los canes.</p>
+            <h3><strong>Agosto</strong></h3>
+            <p>
+                <img src="./img/Desktop-8.png" alt="img" width="600" />
+            </p>
+            <p>P&uacute;blico objetivo: Personas del cualquier sexo que sean amantes de la cerveza. Se eligi&oacute; esta imagen porque representa a una cerveza, su textura y color la hacen llamativa al p&uacute;blico amante de esta.&nbsp;</p>
+            <h3><strong>Septiembre</strong></h3>
+            <p>
+                <img src="./img/Desktop-9.png" alt="img" width="600" />
+            </p>
+            <p>P&uacute;blico objetivo: Personas de nacionalidad salvadore&ntilde;a que se encuentren en el pa&iacute;s como fuera de &eacute;l. Se utiliz&oacute; esta imagen ya que hace mucha referencia a la patria y concuerda con el mes de septiembre en el cual se celebra la independencia del pa&iacute;s.</p>
+            <h3><strong>Octubre</strong></h3>
+            <p>
+                <img src="./img/Desktop-10.png" alt="img" width="600" />
+            </p>
+            <p>P&uacute;blico objetivo: Personas que son amantes del cine de terror como de la lectura y cuentos sobrenaturales. Se utiliz&oacute; esta imagen porque es representativa de la cinem&aacute;tica g&oacute;tica.</p>
+            <h3><strong>Noviembre</strong></h3>
+            <p>
+                <img src="./img/Desktop-11.png" alt="img" width="600" />
+            </p>
+            <p>P&uacute;blico objetivo: Personas que tienen un alto sentido o conexi&oacute;n con sus seres queridos ya en deceso. Se utiliz&oacute; esta imagen ya que hace referencia a la celebraci&oacute;n de tal d&iacute;a con el cual conmemoran a sus seres queridos que han partido de la vida terrenal.</p>
+            <h3><strong>Diciembre</strong></h3>
+            <p>
+                <img src="./img/Desktop-12.png" alt="img" width="600" />
+            </p>
+            <p>P&uacute;blico objetivo: Personas que celebran la navidad con un sentido religioso o bien familiar. Se utiliz&oacute; esta imagen porque es representativa de la celebraci&oacute;n que se lleva a cabo en ese d&iacute;a</p>
+            <p>Gracias por leer.</p>
+        `,
+        img: './img/eventos.png'
+    },
+    {
+        id: 'e5719b52-ef09-4267-95fe-260b01d75020',
+        title: '¿Qué es Node.js?',
+        autor: 'Erick Maldonado',
+        date: '06/03/2021',
+        tags: [tags[2], tags[4]],
+        read: '5 min',
+        likes: 3,
+        content: `
+            <p>es un entorno de trabajo en tiempo de ejecuci&oacute;n, de c&oacute;digo abierto, multi-plataforma, que permite a los desarrolladores crear toda clase de herramientas de lado servidor y aplicaciones en JavaScript. La ejecuci&oacute;n en tiempo real est&aacute; pensada para usarse fuera del contexto de un explorador web, es decir, ejecutarse directamente en una computadora o sistema operativo de servidor.</p>
+            <h2><strong>&iquest;Para qu&eacute; sirve node js?</strong></h2>
+            <p><br />Node js utiliza un modelo de entrada y salida sin bloqueo controlado por eventos que lo hace ligero y eficiente, con entrada nos referimos a solicitudes y con salida a respuestas. Puede referirse a cualquier operaci&oacute;n, desde leer o escribir archivos de cualquier tipo hasta hacer una solicitud HTTP. <br />La idea principal de Node js es usar el modelo de entrada y salida sin bloqueo y controlado por eventos para seguir siendo liviano y eficiente frente a las aplicaciones en tiempo real de uso de datos que se ejecutan en los dispositivos.</p>
+            <h2>&nbsp;</h2>
+            <h2><strong>Ventajas de usar node js</strong></h2>
+            <p>&nbsp;</p>
+            <ul>
+            <li>Gran rendimiento Node ha sido dise&ntilde;ado para optimizar el rendimiento y la escalabilidad en aplicaciones web y es un muy buen complemento para muchos problemas comunes de desarrollo web (ej, aplicaciones web en tiempo real).</li>
+            </ul>
+            <p>&nbsp;</p>
+            <ul>
+            <li>El c&oacute;digo est&aacute; escrito en "simple JavaScript", lo que significa que se pierde menos tiempo ocup&aacute;ndose de las "conmutaciones de contexto" entre lenguajes cuando est&aacute;s escribiendo tanto el c&oacute;digo del explorador web como del servidor.</li>
+            </ul>
+            <p>&nbsp;</p>
+            <ul>
+            <li>El gestor de paquetes Node js proporciona acceso a cientos o miles de paquetes reutilizables tiene adem&aacute;s la mejor en su clase resoluci&oacute;n de dependencias y puede usarse para automatizar la mayor parte de la cadena de herramientas de compilaci&oacute;n.</li>
+            </ul>
+            <p>&nbsp;</p>
+            <ul>
+            <li>Es portable con versiones que funcionan en Microsoft Windows, OS X, Linux, Solaris, FreeBSD, OpenBSD, WebOS, y NonStop OS. Adem&aacute;s, est&aacute; bien soportado por muchos de los proveedores de hospedaje web, que proporcionan infraestructura espec&iacute;fica y documentaci&oacute;n para hospedaje de sitios Node.</li>
+            </ul>
+            <p>&nbsp;</p>
+            <ul>
+            <li>Tiene un ecosistema y comunidad de desarrolladores de terceros muy activa, con cantidad de gente deseosa de ayudar.</li>
+            </ul>           
+        `,
+        img: 'img/nodejs.png'
+    },
     {
         id: '64ac476d-1ffc-499d-9c3f-61bff25f830e',
         title: '¿Cómo instalar el OpenJDK en Ubuntu y derivados?',
